44c1f2ee1dda0cc6b60ffa206c2d44b82bbb560c
SudipGhara19 <sudipghara135@gmail.com>
2024-04-09 01:06:12 +0530

    created jwt.middleware.js to authorize the created token while logIn

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ import express from 'express';
 import productRouter from './src/features/product/product.routes.js';
 import bodyParser from 'body-parser';
 import userRouter from './src/features/user/user.routes.js';
-import basicAuthorizer from './src/middlewares/basicAuth.middleware.js';
+// import basicAuthorizer from './src/middlewares/basicAuth.middleware.js';
+import jwtAuth from './src/middlewares/jwt.middleware.js';
 
 const server = express();
 
@@ -11,7 +12,7 @@ server.use(bodyParser.json());
 
 // for all requests related to product, redirect to product routes.
 // localhost:5500/api/products
-server.use('/api/products', basicAuthorizer, productRouter);
+server.use('/api/products', jwtAuth, productRouter);
 server.use('/api/users', userRouter);
 
 //default request handeler
---
+++ src/middlewares/jwt.middleware.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+
+const jwtAuth = (req, res, next) => {
+
+    //  1. Read The Token
+    const token = req.headers["authorization"];
+
+    // 2. If no token return the error
+    if(!token){
+        return res.status(401).send("unauthorized")
+    }
+
+    // 3. Check if Token is valid
+    try{
+        const payload = jwt.verify(token, "ZrfSDXbpd2Q5eyDtPj5VBUOCpHDHi0Re")
+    }catch(err){
+        // 4. return error
+        return res.status(401).send('Unauthorized');
+    }
+
+    // 5. Call next middleware
+    next();
+
+}
+
+export default jwtAuth;
